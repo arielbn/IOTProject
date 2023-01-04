16dbfa4b76df951ccb70233a409584c0d014055a
Ariel4047 <arielbennatan68@gmail.com>
2023-01-04 10:00:13 +0200

    import files from old

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("myNav").style.width="100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.width="0%";
+}
+const rmCheck = document.getElementById("rememberMe"),
+    emailInput = document.getElementById("email");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+  rmCheck.setAttribute("checked", "checked");
+  emailInput.value = localStorage.username;
+} else {
+  rmCheck.removeAttribute("checked");
+  emailInput.value = "";
+}
+
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value !== "") {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
